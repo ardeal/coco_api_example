7d34ec7d76ea792b0e0ec33875dadbdafc57edd5
Zhiqiang Liang <dpstill@126.com>
2021-04-20 20:08:19 +0800

    init
    init version
    
    Signed-off-by: Zhiqiang Liang <dpstill@126.com>

---
+++ README.md
@@ -0,0 +1,45 @@
+# examples about how to use coco api
+
+
+
+
+# save_coco_json_header() function in the code
+1) this function load coco json file.
+2) you could debug this function and review the data in variable <code>all_coco_data</code>
+3) this fucntion could save the header of coco json for as an example for you to understand what the header of coco is.
+
+# coco_json() function in the code
+the data structure in coco json.
+```python
+ coco_dict = {
+        "info": info,  # dict
+        "licenses": [license],  # list ，a few dicts in the list
+        "images": [image],  # list ，a few dicts in the list
+        "annotations": [annotation],  # list ，a few dicts in the list
+        "categories": category # list ，a few dicts in the list
+    }
+```
+
+# coco_image_example() function in the code
+the code shows how to read 1 image and its annotation from coco and show them.
+
+
+
+# data structure of coco json
+the following picture shows the data structure of coco json. it explains image_id and annotation_id.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ coco_api_example.py
@@ -0,0 +1,70 @@
+
+import os
+import json
+
+
+from pycocotools.coco import COCO
+import matplotlib.pyplot as plt
+import cv2
+
+
+def save_coco_json_header():
+    coco_json = r"D:\datasets\coco\images\instances_train2014.json"
+    with open(coco_json) as f:
+        all_coco_data = json.load(f)
+    info = all_coco_data['info']
+    license = all_coco_data['licenses']
+    json.dump({'info':info, 'licenses':license}, open('json_coco_header.json', 'w'))
+
+    return
+
+def coco_json():
+
+    info = dict()
+    license = [dict()]
+    image = [dict()]
+    annotation = [dict()]
+    category = [dict()]
+
+    coco_dict = {
+        "info": info,  # dict
+        "licenses": [license],  # list ，a few dicts in the list
+        "images": [image],  # list ，a few dicts in the list
+        "annotations": [annotation],  # list ，a few dicts in the list
+        "categories": category # list ，a few dicts in the list
+    }
+    print(coco_dict)
+
+    return
+
+def coco_image_example():
+    cocodataset = COCO(r"D:\datasets\coco\images\instances_train2014.json")
+    image0_id = cocodataset.imgs[57870]['id']
+    for key, val in cocodataset.imgs.items():
+        image_dir = r'D:\datasets\coco\images\train2014'
+        imageinfo =  cocodataset.loadImgs(val['id'])
+        image = cv2.imread(os.path.join(image_dir, imageinfo[0]['file_name']))
+
+        plt.imshow(image)
+        plt.axis('off')
+        anns = cocodataset.imgToAnns[val['id']]
+        cocodataset.showAnns(anns)
+        plt.show()
+
+
+        aaaaaa = 0
+
+        # anns_image_id = cocodataset.anns[key]['image_id']
+        # if anns_image_id == image0_id:
+        #     print('found anns key =={}, anns_id = {}, for image0_id ={}'.format(key, val['id'], image0_id))
+
+
+if __name__ == '__main__':
+
+    # ------------------------->
+    save_coco_json_header()
+    coco_image()
+
+
+    aaaaaaaaaaaaaaaaa=0
+
